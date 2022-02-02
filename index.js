f992e617caf857fc1cb249d22df145a28dbe33c5
markobilanovic <bilanovic90@gmail.com>
2022-02-02 21:28:51 +0100

    test

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const puppeteer = require('puppeteer');
 const mailService = require('./mailService');
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(process.env.PORT || 4000, function () {
+    console.log('Node app is working!');
+});
 
 const db = [
   {
