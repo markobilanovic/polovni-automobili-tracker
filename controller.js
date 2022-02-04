a666dcbb8976f5e12f60212e2c7622e4b8cda0d7
markobilanovic <bilanovic90@gmail.com>
2022-02-04 23:59:05 +0100

    aaa

--- controller.js
+++ controller.js
@@ -3,6 +3,9 @@ var app = express();
 var path = require('path');
 const db = require('./db');
 
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded()); // to support URL-encoded bodies
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
